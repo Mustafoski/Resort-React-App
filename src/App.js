d9c2317bfec41ef9d748b8d2a3a10c34152a5eaf
Emir <emirmustafoski92@gmail.com>
2020-10-05 16:05:21 +0200

    Initilizing pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
 export default class App extends Component {
   render() {
-    return <React.Fragment>Hello From App</React.Fragment>;
+    return (
+      <React.Fragment>
+        <Home />
+        <Rooms />
+        <SingleRoom />
+        <Error />
+      </React.Fragment>
+    );
   }
 }
